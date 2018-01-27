592a07f17fd871ef821a2bf9339b169e93941c98
Jack <jack@thinkingcloud.info>
2018-01-27 21:29:59 +0800

    Added the define and undefine support to the Macro.

--- spec/macro/MacroEngineSpec.js
+++ spec/macro/MacroEngineSpec.js
@@ -1,6 +1,6 @@
 const notMatchString = `#if 1 > 2
 Hello
-#if
+#if 1 < 2
 #else
 World
 #endif`;
@@ -11,15 +11,109 @@ Hello
 World
 #endif`;
 
-const complexString = `#if 1 > 2
-Hello
+const complexString = `#if global.DEBUG
+#if global.ANOTHER_INT > 20
+DEBUG 20
 #else
-World
+DEBUG 0
+#endif
+#else
+NO DEBUG
 #endif`;
 
-describe("Macro Engine Test Suite", function() {
-	it("Macro Engine Simple Process Test", function() {
-		let { Configurator: { MacroEngine: { basic } }} = this;
+const complexStringWithIfDef = `#if global.DEBUG
+#ifdef global.TEST
+TEST
+#else
+NOT TEST
+#endif
+#else
+NO DEBUG
+#endif`;
+
+const simpleDefineString = `
+#define TEST 1
+`
+
+const complexDefineString = `#if global.DEBUG
+#define MY_DEBUG
+#undefine NO_DEBUG
+#else
+#define NO_DEBUG
+#undefine MY_DEBUG
+#endif
+#ifdef process.env.MY_DEBUG
+DEBUG
+#endif
+#ifdef process.env.NO_DEBUG
+NO DEBUG
+#endif`;
+
+
+global.DEBUG = 1;
+global.ANOTHER_INT = 23;
+
+describe("MacroEngine", function() {
+	it("Simple Process Test", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
         basic.process(simpleString).then(txt => expect(txt).toBe("World")).catch(fail);
 	});
+
+    it("Not Match Test", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        basic.process(notMatchString).then(() => fail("Didn't get error thrown!")).catch(e => expect(e).toBeTruthy());
+    });
+
+    it("Complex Process Test", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        basic.process(complexString).then(txt => expect(txt).toBe("DEBUG 20")).catch(fail);
+    });
+
+    it("Complex Process Test For Branch", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        global.ANOTHER_INT = 1;
+        basic.process(complexString).then(txt => expect(txt).toBe("DEBUG 0")).catch(fail);
+    });
+
+    it("Complex Process Test For If Defined", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        global.DEBUG = 1;
+        basic.process(complexStringWithIfDef).then(txt => expect(txt).toBe("NOT TEST")).catch(fail);
+    });
+
+    it("Complex Process Test For If Defined 2", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        global.DEBUG = 1;
+        global.TEST = 1;
+        basic.process(complexStringWithIfDef).then(txt => expect(txt).toBe("TEST")).catch(fail);
+    });
+
+    it("Complex Process Test For Global Branch", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        global.DEBUG = 0;
+        basic.process(complexString).then(txt => expect(txt).toBe("NO DEBUG")).catch(fail);
+    });
+
+    it("Define Test", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        basic.process(simpleDefineString).then(() => expect(process.env.TEST).toBe("1")).catch(fail);
+    });
+
+    it("Define Complex Test 1", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        global.DEBUG = 0;
+        basic.process(complexDefineString).then(txt => {
+            expect(process.env.MY_DEBUG).toBeFalsy();
+            expect(txt).toBe("NO DEBUG");
+        }).catch(fail);
+    });
+
+    it("Define Complex Test 1", function() {
+		let { Configurator: { MacroEngine: { basic } } } = this;
+        global.DEBUG = 1;
+        basic.process(complexDefineString).then(txt => {
+            expect(process.env.MY_DEBUG).toBeTruthy();
+            expect(txt).toBe("DEBUG");
+        }).catch(fail);
+    });
 })
--- src/macro/MacroEngine.js
+++ src/macro/MacroEngine.js
@@ -8,7 +8,7 @@
  */
 
 const FilterBase = require("../models/FilterBase");
-const { isString, isArray } = require("lodash");
+const { flatten, isString, isArray } = require("lodash");
 const { FilterObject } = FilterBase;
 
 /**
@@ -17,8 +17,11 @@ const { FilterObject } = FilterBase;
 const IF_STATE = "if";
 const ELSE_STATE = "else";
 const IF_PATTERN = /^(\\s)*#if (.+)$/;
+const IF_DEF_PATTERN = /^(\\s)*#ifdef (.+)$/;
 const ELSE_PATTERN = /^(\\s)*#else$/;
 const END_IF_PATTERN = /^(\\s)*#endif$/;
+const DEFINE_PATTERN = /^(\\s)*#define ([a-z\\.A-Z_]+)( (.+))?/;
+const UNDEFINE_PATTERN = /^(\\s)*#undefine ([a-z\\.A-Z_]+)/;
 
 class TextFilterBase extends FilterObject {
     constructor(name) {
@@ -71,8 +74,46 @@ class JoinLinesFilter extends FilterObject {
     }
 }
 
-class IfBlock {
+/**
+ * The base class for all blocks
+ */
+class Block {
+    /**
+     * Let the block process the value of its own
+     */
+    process() {
+    }
+}
+
+class DefineBlock extends Block {
+    constructor(variable, value = true) {
+        super();
+        this.variable = variable;
+        this.value = value;
+    }
+
+    process() {
+        process.env[this.variable] = this.value;
+    }
+}
+
+class UndefineBlock extends Block {
+    constructor(variable) {
+        super();
+        this.variable = variable;
+    }
+
+    process() {
+        if(this.variable &&
+            typeof process.env[this.variable] !== "undefined") {
+            delete process.env[this.variable];
+        }
+    }
+}
+
+class IfBlock extends Block {
     constructor(expression) {
+        super();
         this._expression = expression;
         this._state = IF_STATE;
         this._block = [];
@@ -83,14 +124,18 @@ class IfBlock {
         this._state = state;
     }
 
-    values() {
+    expr() {
+        return eval(`(${this._expression})`);
+    }
+
+    process() {
         if(this._expression) {
-            if(!!eval(`(${this._expression})`)) {
-                return this._block.map(b => b instanceof IfBlock? b.values(): b);
+            if(!!this.expr()) {
+                return flatten(this._block.map(b => b instanceof Block? b.process(): b).filter(i=>!!i));
             }
         }
 
-        return this._alternative.map(b => b instanceof IfBlock? b.values(): b);
+        return flatten(this._alternative.map(b => b instanceof Block? b.process(): b).filter(i=>!!i));
     }
 
     add(line) {
@@ -107,10 +152,66 @@ class IfBlock {
     }
 }
 
+class IfDefBlock extends IfBlock {
+    constructor(variable) {
+        super(`typeof ${variable} !== "undefined"`);
+    }
+}
+
+class DefineFilter extends TextFilterBase {
+    constructor(name) {
+        super(name);
+    }
+
+    filter(text = []) {
+        let ret = [];
+        for(let t of text) {
+            let m = t.match(DEFINE_PATTERN);
+            if(m) {
+                let name = m[2];
+                let value = m[4] || true;
+                // OK, let's set the value to the global
+                if(name) {
+                    process.env[name] = value;
+                }
+            } else {
+                // This is simple text, let's skip it
+                ret.push(t);
+            }
+        }
+        return ret;
+    }
+}
+
+const handleDefine = (t) => {
+    let m = t.match(DEFINE_PATTERN);
+    if(m) {
+        // This is a define operation
+        let name = m[2];
+        let value = m[4] || true;
+        // OK, let's set the value to the process.env only
+        if(name) {
+            return new DefineBlock(name, value);
+        }
+    }
+
+    m = t.match(UNDEFINE_PATTERN);
+    if(m) {
+        // This is a undefine operation
+        let name = m[2];
+        if(name) {
+            return new UndefineBlock(name);
+        }
+    }
+
+    return t;
+}
+
 /**
- * The filter will support the #if and #else
+ * The filter will support the #if and #else, and since ifdef will need define
+ * and undefine, so, the define patterns will be handle in this filter too
  */
-class IfFilter extends TextFilterBase {
+class CoreFilter extends TextFilterBase {
     constructor(name) {
         super(name);
     }
@@ -135,7 +236,7 @@ class IfFilter extends TextFilterBase {
                     stack.push(currentBlock);
 
                     // Then add the block into the current block
-                    currentBlock.push(block);
+                    currentBlock.add(block);
                 }
 
                 // Update the current block using the new one
@@ -144,6 +245,26 @@ class IfFilter extends TextFilterBase {
                 // Then move to next line
                 continue;
             }
+
+            m = t.match(IF_DEF_PATTERN);
+            if(m) {
+                let block = new IfDefBlock(m[2]);
+
+                if(currentBlock) {
+                    // Push the old block into the stack first
+                    stack.push(currentBlock);
+
+                    // Then add the block into the current block
+                    currentBlock.add(block);
+                }
+
+                // Update the current block using the new one
+                currentBlock = block;
+
+                // Then move to next line
+                continue;
+            }
+
 
             if(t.match(ELSE_PATTERN)) {
                 // This is an else line, let's update the current block
@@ -166,7 +287,7 @@ class IfFilter extends TextFilterBase {
                     currentBlock = stack.pop();
                 } else {
                     // We don't have any blocks in the stack, this means this block is the topmost block, let's add its values to the result
-                    ret = ret.concat(currentBlock.values());
+                    ret = ret.concat(currentBlock.process());
                     // Then, let's update the current block to null again
                     currentBlock = null;
                 }
@@ -175,17 +296,39 @@ class IfFilter extends TextFilterBase {
                 continue;
             }
 
+            let b = handleDefine(t);
+
             if(currentBlock) {
                 // If there is a block, let's add it into the current block
-                currentBlock.add(t);
+                currentBlock.add(b);
             } else {
+                if(b instanceof Block) {
+                    // If this is a block, let's process it
+                    t = b.process();
+                    if(!t) {
+                        // If there is nothing, let's move to next line
+                        continue;
+                    }
+
+                    if(isArray(t)) {
+                        // If the result is an array, let's put them all together into the return
+                        ret = ret.concat(t);
+                        // Move to next line
+                        continue;
+                    }
+
+                    if(!isString(t)) {
+                        t = new String(t);
+                    }
+                }
+
                 // This is not any if macro, and there is no block here let's just add it into the ret
                 ret.push(t);
             }
         }
 
-        if(stack.length) {
-            throw new Error("The text's text didn't have #if and the #endif match, you should check the text!");
+        if(stack.length || !!currentBlock) {
+            throw new Error("The text didn't have #if and the #endif match, you should check the text!");
         }
         return ret;
     }
@@ -208,9 +351,10 @@ class MacroEngine extends FilterBase {
 }
 
 MacroEngine.TextFilterBase = TextFilterBase;
-MacroEngine.IfFilter = IfFilter;
+MacroEngine.CoreFilter = CoreFilter;
+
 MacroEngine.basicFilters = [
-    new IfFilter()
+    new CoreFilter("core-filter")
 ];
 MacroEngine.basic = new MacroEngine(MacroEngine.basicFilters);
 
